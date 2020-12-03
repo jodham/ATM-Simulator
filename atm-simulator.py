45d7330fdea93bde993d5fcc80a8b8fbb2491f47
wanjalajodham@gmail.com <jodham>
2020-12-03 20:58:26 +0300

    update

--- atm-simulator.py
+++ atm-simulator.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-class account:
+class Account:
     def __init__(self,id, checkingBalance= 0, savingBalance= 0, annualInterestRateSavings= 3.4):
         self.id = id
         self.checkingBalance = checkingBalance
@@ -33,3 +33,26 @@ class account:
             return self.annualInterestRateSavings
         def savingsAccountMonthlyInterestRate(self):
             return self.annualInterestRateSavings /12
+def main():
+    #creating accounts
+    accounts = []
+    for i in range(1000, 9999):
+        account = Account(i, 0)
+        accounts.append(account)
+
+        while True:
+            #reading id from user
+            print("welcome to wanjas bank.")
+            id = int( input("\nEnter 4 digit account pin: "))
+
+            #loop till id is valid
+            while id < 1000 or id > 9999:
+                id = int(input("\nInvald Id..Re-Enter: "))
+                #iterating over the interface
+            while True:
+
+                print("\nHow can we help you today?")
+                print("""\n - View checking balance\t\t2 \n3 - Deposit checking \t\t\t4 - transfer Checking \n5 - View 
+                Savings Balance \t\t6 - Withdraw Savings \n7 - Deposit Savings \t\t\t8 - Transfer Savings \9n - Exit """)
+
+
