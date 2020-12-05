2d0ad3814828ff0310ab4a91bf476321eb6f7c32
wanjalajodham@gmail.com <jodham>
2020-12-05 07:15:01 +0300

    update

--- transaction_page.py
+++ transaction_page.py
@@ -2,6 +2,7 @@ import random
 
 amount = 0.0
 loggedOn = True
+transactionAmount = 23
 
 while loggedOn:
     selection = int(input("Select 1 for Deposit, 2 for withdraw or 3 for Exit"))
@@ -14,16 +15,17 @@ while loggedOn:
         print(f"Bank account balance ${format(amount, '.2f')} ")
     elif selection == 2:
         withdraw = float(input("How much will you withdraw?"))
-        if amount >= withdraw:
-            amount -= withdraw
+        if amount >= withdraw + transactionAmount:
+            amount -= withdraw + transactionAmount
             print(f"\nWithdraw in the amount of ${format(amount, '.2f')}")
             print(f"Bank account balance ${format(amount, '.2f')}")
         else:
-            print("\nThis transaction cannot be completed")
-            print("your balance",+amount, "is less than requested amount")
+            print("\nUnsuccessful!!!\nUnable to withdraw",withdraw, "\nInsufficient funds\nYou haveto cater for transaction charges too\nYour balance is",amount)
+
     else:
         loggedOn = False
         print("\nTransaction id :", random.randint(10000, 1000000))
+        print("Thanks for choosing our bank see u soon")
 
 
 
