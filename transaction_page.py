9ae3380cc83ecb713bc8112e965f39f29b3f7f8e
wanjalajodham@gmail.com <jodham>
2020-12-05 22:50:11 +0300

    update

--- transaction_page.py
+++ transaction_page.py
@@ -2,10 +2,11 @@ import random
 
 amount = 0.0
 loggedOn = True
-transactionAmount = 23
+withdrawalAmount = 23
+sendingCharges = 15
 
 while loggedOn:
-    selection = int(input("Select 1 for Deposit, 2 for withdraw or 3 for Exit"))
+    selection = int(input("Select 1 for Deposit, 2 for withdraw ,3 for send money or 4 for Exit"))
     if not selection:
         break
     if selection == 1:
@@ -15,17 +16,34 @@ while loggedOn:
         print(f"Bank account balance ${format(amount, '.2f')} ")
     elif selection == 2:
         withdraw = float(input("How much will you withdraw?"))
-        if amount >= withdraw + transactionAmount:
-            amount -= withdraw + transactionAmount
+        if amount >= withdraw + withdrawalAmount:
+            amount -= withdraw + withdrawalAmount
             print(f"\nWithdraw in the amount of ${format(amount, '.2f')}")
             print(f"Bank account balance ${format(amount, '.2f')}")
         else:
-            print("\nUnsuccessful!!!\nUnable to withdraw",withdraw, "\nInsufficient funds\nYou haveto cater for transaction charges too\nYour balance is",amount)
+            print("\nUnsuccessful!!!\nUnable to withdraw", withdraw,
+                  "\nInsufficient funds\nYou haveto cater for transaction charges too\nYour balance is", amount)
+    elif selection == 3:
+        phoneNumber = int(input("Enter recievers number"))
+        if phoneNumber == 10:
+            sendMoney = float(input("Enter amount to send"))
+            if amount >= sendMoney + sendingCharges:
+                amount -= sendMoney + sendingCharges
+                print(f"\nSend in the amount of ${format(sendMoney, '.2f')}")
+                print(f"Bank account balance is ${format(amount, '.2f')}")
+            else:
+                print("Invalid Number.........try again.")
+
+
 
     else:
         loggedOn = False
         print("\nTransaction id :", random.randint(10000, 1000000))
         print("Thanks for choosing our bank see u soon")
+
+
+
+
 
 
 
