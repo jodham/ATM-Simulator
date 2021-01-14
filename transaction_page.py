fa214acbdf4c559a54d562c0009b54e8133e1fd9
wanjalajodham@gmail.com <wanjalajodham@gmail.com>
2021-01-14 13:27:05 +0300

    update

--- transaction_page.py
+++ transaction_page.py
@@ -10,7 +10,7 @@ while loggedOn:
     if not selection:
         break
     if selection == 1:
-        deposit = float(input("How much will you deposit"))
+        deposit = float(input("How much will you deposit:"))
         amount += deposit
         print(f"Deposit in the amount of ${format(deposit, '.2f')} ")
         print(f"Bank account balance ${format(amount, '.2f')} ")
@@ -24,16 +24,17 @@ while loggedOn:
             print("\nUnsuccessful!!!\nUnable to withdraw", withdraw,
                   "\nInsufficient funds\nYou haveto cater for transaction charges too\nYour balance is", amount)
     elif selection == 3:
-        phoneNumber = int(input("Enter receivers number"))
+        phoneNumber = str(input("Enter1"" receivers number"))
         sendMoney = int(input("Enter amount to send"))
-        print(f"\nsend money in the amount of ${format(sendMoney, '.2f')}")
-        if phoneNumber == 10 and amount >= sendMoney + sendingCharges:
+        if (len(phoneNumber) ==10 ) and (amount >= sendMoney + sendingCharges):
             amount -= sendMoney + sendingCharges
+            print(f"\nsend money in the amount of ${format(sendMoney, '.2f')} to", phoneNumber, )
             print(f"\nYour Balance is ${format(amount, '.2f')}")
-        elif amount < sendMoney + sendingCharges:
-            print(f"insufficient balance\nYour balance is ${format(amount, '.2f')}")
-        #else:
-           #print("invalid number.......try again")
+        elif len(phoneNumber) != 10:
+            print("incorrect phone number")
+        else:
+            print(f"insufficient balance\nyour balance is ${format(amount, '.2f')}")
+
     else:
         loggedOn = False
         print("\nTransaction id :", random.randint(10000, 1000000))
