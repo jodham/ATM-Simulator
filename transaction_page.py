eb3f16988d86c13b3d8a1fc55cdc31c8fc2a6d0b
wanjalajodham@gmail.com <wanjalajodham@gmail.com>
2021-01-15 17:02:34 +0300

    update

--- transaction_page.py
+++ transaction_page.py
@@ -10,12 +10,12 @@ while loggedOn:
     if not selection:
         break
     if selection == 1:
-        deposit = float(input("How much will you deposit:"))
+        deposit = float(input("How much will you deposit :"))
         amount += deposit
         print(f"Deposit in the amount of ${format(deposit, '.2f')} ")
         print(f"Bank account balance ${format(amount, '.2f')} ")
     elif selection == 2:
-        withdraw = float(input("How much will you withdraw?"))
+        withdraw = float(input("How much will you withdraw? :"))
         if amount >= withdraw + withdrawalAmount:
             amount -= withdraw + withdrawalAmount
             print(f"\nWithdraw in the amount of ${format(amount, '.2f')}")
@@ -24,12 +24,12 @@ while loggedOn:
             print("\nUnsuccessful!!!\nUnable to withdraw", withdraw,
                   "\nInsufficient funds\nYou haveto cater for transaction charges too\nYour balance is", amount)
     elif selection == 3:
-        phoneNumber = str(input("Enter1"" receivers number"))
+        phoneNumber = str(input("Enter receivers number :"))
         sendMoney = int(input("Enter amount to send"))
         if (len(phoneNumber) ==10 ) and (amount >= sendMoney + sendingCharges):
             amount -= sendMoney + sendingCharges
-            print(f"\nsend money in the amount of ${format(sendMoney, '.2f')} to", phoneNumber, )
-            print(f"\nYour Balance is ${format(amount, '.2f')}")
+            print(f"\nsend money in the amount of ${format(sendMoney, '.2f')} to", phoneNumber,)
+            print(f"\nYour Balance is ${format(amount, '.2f')}\ntransaction cost ${format(sendingCharges, '.2f')}")
         elif len(phoneNumber) != 10:
             print("incorrect phone number")
         else:
